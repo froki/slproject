c919a6f5dfcf817b3c26bd0fe42207abf772b56f
Alexander Stangel <alexanderstangel@gmail.com>
2013-10-21 15:16:50 +0200

    working JSON fetch

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-$(document).ready(function(){
-  console.log('ouybouy')
-function hejtest() {
+$(document).ready(function() {
+var url ='https://free-ec2.scraperwiki.com/cvdpl6y/ebf52a60f3514fc/sql/?q=select%20%0A%09id_str%2C%0A%09tweet_url%2C%0A%09created_at%2C%0A%20%20%20%20text%2C%0A%20%20%20%20hashtags%0Afrom%20tweets%0A--%20where%20created_at%20%3E%20%0Aorder%20by%20tweet_url%0Alimit%2020'
+$.getJSON(url, whatever);
 
-  var twitterAPI = "https://free-ec2.scraperwiki.com/cvdpl6y/ebf52a60f3514fc/sql/?q=select%20%0A%09id_str%2C%0A%09tweet_url%2C%0A%09created_at%2C%0A%20%20%20%20text%2C%0A%20%20%20%20hashtags%0Afrom%20tweets%0A--%20where%20created_at%20%3E%20%0Aorder%20by%20tweet_url%0Alimit%2020%0A";
-  $.getJSON( twitterAPI, {
-  });
-  
-};
 
-hejtest();
-  
+function whatever(data) {
+var i = 0;
+for ( var i = 0; i < data.length; i++){
+ console.log(data[i].text);
+}}
 });
