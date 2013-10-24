7c9ae6ed6961d29be3f14111f923109e798bf1fc
froki <danijela.froki@hotmail.com>
2013-10-24 12:10:07 +0200

    Daniels Magic

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
 $(document).ready(function() {
-var url ='https://free-ec2.scraperwiki.com/cvdpl6y/ebf52a60f3514fc/sql/?q=select%20%0A%09id_str%2C%0A%09tweet_url%2C%0A%09created_at%2C%0A%20%20%20%20text%2C%0A%20%20%20%20hashtags%0Afrom%20tweets%0A--%20where%20created_at%20%3E%20%0Aorder%20by%20tweet_url%0Alimit%2020'
-$.getJSON(url, whatever);
+	var url ='https://free-ec2.scraperwiki.com/c4uem5y/b800cc5ce96d4be/sql/?q=select%20%0A%09id_str%2C%0A%09tweet_url%2C%0A%20%20%20%20text%2C%0A%20%20%20%20tweet_url%2C%0A%20%20%20%20created_at%0Afrom%20tweets%0A--%20where%20created_at%20%3E%20%0Aorder%20by%20created_at%0A'
+	$.getJSON(url, processTweets);
 
 
-function whatever(data) {
-var i = 0;
-for ( var i = 0; i < data.length; i++){
- // console.log(data[i].text);
- response = data[i].text; 
- response.toString();
- lookUp = "kista";
+	function processTweets(data) {
+		for (var i = 0; i < data.length; i++){
+			response = data[i].text; 
+			response = response.toLowerCase();
 
+			for (var station in stations) {
+				if (response.indexOf(station) != -1) {
+					showMarker(station);
+				}
+			}
 
-if (response.indexOf(lookUp) != -1)
-{
- console.log("Works");
-
-}
-
-}}
+		}
+	}
 });
 
+function showMarker(station) {
+	var pin = jQuery("<div>");
+	pin.addClass("pin");
+	pin.css("left", stations[station].x + "%");
+	pin.css("top", stations[station].y + "%");
+	pin.append("<img src='SLmappin.png'>")
+	pin.appendTo("#map");
+}
 
 
 stations = {
-	fridhemsplan: { x: 133, y: 321 },
-	telefonplan: { x: 1331, y: 1955 }
-	midsommarkransen: { x: 1511, y: 1545 }
-
-	
+	fridhemsplan: { x: 55.5, y: 44.6 },
+	telefonplan: { x: 37.7, y: 75.1 }	
 }
